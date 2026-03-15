0fb4325404b57f3e74938c6473ad101a05b626ca
Kaito1412Hung <hungquangkid14121412@gmail.com>
2026-03-15 21:41:46 +0700

    update code

--- Week2_Reactjs/react-todolist/src/components/TodoList.jsx
+++ Week2_Reactjs/react-todolist/src/components/TodoList.jsx
@@ -1,10 +1,10 @@
 import React from 'react'
 import TodoItem from './TodoItem';
 
-function TodoList({todo, onDelete}) {
+function TodoList({todos, onDelete}) {
     return (
         <ul className="todo-list"> 
-            {todo.map((todo) => {
+            {todos.map((todo) => {
                 return <TodoItem key={todo.id} todo={todo} onDelete={onDelete} />
             })}
         </ul>
